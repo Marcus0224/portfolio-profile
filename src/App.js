0d31a91ab5044eb1f1f3a51f0da1ef7c3d42d1d3
Marcus0224 <marcstlouis04@gmail.com>
2022-06-09 17:25:42 -0400

    footer files excuted

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import React from 'react';
+
+import React, { useState } from 'react';
 import './App.css';
+import Nav from './components/Nav';
+import About from './components/About';
+import Contact from './components/Contact';
+import Projects from './components/Projects';
+import Resume from './components/Resume';
+import Footer from './components/Footer';
 
 function App() {
+  const categories = ['About me', 'Projects', 'Contact', 'Resume'];
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
   return (
     <div>
-      
+      <Nav 
+        categories={categories}
+        currentCategory={currentCategory}
+        setCurrentCategory={setCurrentCategory}
+      />
+      {currentCategory === 'About me' && <About />}
+      {currentCategory === 'Projects' && <Projects />}
+      {currentCategory === 'Contact' && <Contact />}
+      {currentCategory === 'Resume' && <Resume />}
+      <Footer />
     </div>
   );
 }
