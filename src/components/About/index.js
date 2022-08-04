2913a8fe27dba34cf7aeda309c3753e1bbe1eabd
Marcus0224 <marcstlouis04@gmail.com>
2022-08-04 19:30:06 -0400

    fixed about me portion

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,32 +1,55 @@
 import React from "react";
+import coverImage from '../../assets/cover/cover-image.jpg';
 import './about.css';
+import {BsAward} from 'react-icons/bs'
+import { FiUsers} from 'react-icons/fi'
+import {VscFolder} from 'react-icons/vsc'
 
-function About() {
+
+const About = () => {
   return (
-    <section className="mx-4 about">
-      <h1 className="text-align">Marc St louis</h1>
-      <div className="d-flex flex-wrap about-container mt-1">
-        <div className="flex-row bio-img-container">
-          <img
-            className="img-fluid rounded bio-img"
-            src="./assets/bio-img.jpeg"
-         ></img>
+    <section id='about'>
+      <h5>Get to Know</h5>
+      <h2>About Me</h2>
+      
+      <div className="container about_container">
+        <div className="about_me">
+          <div className="about_me-image">
+            <img src={coverImage} alt='About me' />
+          </div>
         </div>
-        <div className="col-md-8 flex-row bio m-auto">
+        <div className="about_content">
+          <div className="about_cards">
+            <article className='about_card'>
+              <BsAward className='about_icon'/>
+              <h5>
+                Expreience
+              </h5>
+              <small>Junior Fullstack Developer</small>
+            </article>
+            <article className='about_card'>
+              <FiUsers className='about_icon'/>
+              <h5>
+                Clients
+              </h5>
+              <small>TBD</small>
+            </article>
+            <article className='about_card'>
+              <VscFolder className='about_icon'/>
+              <h5>
+                Projects
+              </h5>
+              <small>20+ Completed Projects</small>
+            </article>
+          </div>
           <p>
-            {" "}
-            Im a FullStack developer looking for an opportunity to connect and
+          Im a FullStack developer looking for an opportunity to connect and
             showcase my abilities. I have less than a year experience in coding
             but willing to learn and grow within the right organzation. Im a
             proven problem solver with great communication skills to help any
             organzation succeed.
           </p>
-          <p>
-            {" "}
-            When im not prefecting my coding skills, I like to travel the world
-            andn learn from the natives of tge country. I so like to workout and
-            stay in physical as well as mental shape.
-          </p>
+          <a href='#contact' className='btn btn-primary'>Let's Connect</a>
         </div>
       </div>
     </section>
